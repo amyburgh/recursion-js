741e5cd99bfe9f24d42e7d8488507c3c84b5c94e
amyburgh <air.myburgh@gmail.com>
2023-06-19 16:51:48 +0200

    fix tabs

--- assignment-1.js
+++ assignment-1.js
@@ -1,21 +1,21 @@
 function fibsIter(n) {
-	if (n <= 1) return [0]
-	const array = [0, 1]
+  if (n <= 1) return [0]
+  const array = [0, 1]
 
-	for (let i = 2; i < n; i++) {
-		array.push(array.at(-1) + array.at(-2))
-	}
-	return array
+  for (let i = 2; i < n; i++) {
+    array.push(array.at(-1) + array.at(-2))
+  }
+  return array
 }
 
 function fibsRec(n, arr = []) {
-	if (n <= 1) return [0]
-	if (n === 2) return [0, 1]
+  if (n <= 1) return [0]
+  if (n === 2) return [0, 1]
 
-	const array = fibsRec(n - 1)
-	const val = array.at(-1) + array.at(-2)
-	array.push(val)
-	return array
+  const array = fibsRec(n - 1)
+  const val = array.at(-1) + array.at(-2)
+  array.push(val)
+  return array
 }
 
 console.log(fibsIter(8))
