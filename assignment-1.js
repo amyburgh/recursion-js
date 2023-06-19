706a3271206f941010d6833136fee64eaf83ed07
amyburgh <air.myburgh@gmail.com>
2023-06-19 16:38:39 +0200

    initial commit

---
+++ assignment-1.js
@@ -0,0 +1,22 @@
+function fibsIter(n) {
+	if (n <= 1) return [0]
+	const array = [0, 1]
+
+	for (let i = 2; i < n; i++) {
+		array.push(array.at(-1) + array.at(-2))
+	}
+	return array
+}
+
+function fibsRec(n, arr = []) {
+	if (n <= 1) return [0]
+	if (n === 2) return [0, 1]
+
+	const array = fibsRec(n - 1)
+	const val = array.at(-1) + array.at(-2)
+	array.push(val)
+	return array
+}
+
+console.log(fibsIter(8))
+console.log(fibsRec(8))
---
+++ mergeSort.js
@@ -0,0 +1,24 @@
+// Time Complexity: O(n log n)
+// Space complexity: O(n)
+function merge(a, b) {
+	const arr = []
+
+	while (a.length && b.length) {
+		if (a[0] < b[0]) arr.push(a.shift())
+		else arr.push(b.shift())
+	}
+	return [...arr, ...a, ...b]
+}
+
+function mergeSort(array) {
+	if (array.length < 2) return array
+	const mid = array.length / 2
+	const left = array.slice(0, mid)
+	const right = array.slice(mid)
+
+	return merge(mergeSort(left), mergeSort(right))
+}
+
+const array = [3, 1, 5, 8, 2, 7, 9, 4, 6, 0]
+const sorted = mergeSort(array)
+console.log(array, sorted)
